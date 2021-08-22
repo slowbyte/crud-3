d5ebe9f8abaf8ec0d50989445b2d47d803e11e78
slowbyte <slowbyte1@gmail.com>
2021-08-22 13:43:27 -0400

    fixed App.js {useStatae to {useState

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {useStatae} from 'react';
+import {useState} from 'react';
 import axios from 'axios';
 import ComponentC from './ComponentC';
 
